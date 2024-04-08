9c1fe1257553c5a550adbb75b24800d675616397
Ivan Shivalov <142020129+intexcor@users.noreply.github.com>
2024-04-08 16:43:34 +0300

    beta

--- server.py
+++ server.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+import os
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from flask import Flask
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+app = Flask(__name__)
 
 
-# Press the green button in the gutter to run the script.
+@app.route("/")
+def index():
+    return "Привет от приложения Flask"
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
